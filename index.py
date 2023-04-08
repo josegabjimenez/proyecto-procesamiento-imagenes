7d88b2c15b6c2f6c7bac669f27255b0516445104
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-04-07 19:40:36 -0500

    feat: add thresholding and displaying image

--- index.py
+++ index.py
@@ -11,23 +11,65 @@ image_input = st.text_input("Ingresa el nombre de la imagen")
 image_data = nib.load(f'./images/1/{image_input}.gz')
 image = image_data.get_fdata()
 
-segmentation_options = ['Thresholding', 'Region Growing', 'K-means']
-selected_segmentation_option = st.radio('Selecciona una técnica de segmentación', segmentation_options)
-
 # Create three columns for the inputs
 col1, col2, col3 = st.columns(3)
 
 # Add a numeric input field to each column
 with col1:
-    input1 = st.number_input("Input 1", value=0, step=1)
+    axisX = st.number_input("Eje X", value=0, step=1)
 with col2:
-    input2 = st.number_input("Input 2", value=0, step=1)
+    axisY = st.number_input("Eje Y", value=0, step=1)
 with col3:
-    input3 = st.number_input("Input 3", value=0, step=1)
+    axisZ = st.number_input("Eje Z", value=0, step=1)
+
+if sum([axisX <= -1, axisY <= -1, axisZ <= -1]) != 2:
+    st.warning("Dos ejes deben tener exáctamente el valor de -1")
+
+if axisX == -1:
+    axisX = slice(None)
+if axisY == -1:
+    axisY = slice(None)
+if axisZ == -1:
+    axisZ = slice(None)
+
+segmentation_options = ['Thresholding', 'Region Growing', 'K-means']
+selected_segmentation_option = st.radio('Selecciona una técnica de segmentación', segmentation_options)
+
+# Create segmentation button
+segmentation_button_clicked = st.button("Crear segmentación")
+
+# Algorithms
+def thresholding(image, tol = 1, tau = 20):
+
+    while True:
+        # print(tau)
+
+        segmentation = image >= tau
+        mBG = image[np.multiply(image > 10, segmentation == 0)].mean()
+        mFG = image[np.multiply(image > 10, segmentation == 1)].mean()
+
+        tau_post = 0.5 * (mBG + mFG)
+
+        if np.abs(tau - tau_post) < tol:
+            break
+        else:
+            tau = tau_post
+
+    return segmentation
+
 
 if selected_segmentation_option == 'Thresholding':
+    # Create the plot using imshow
+    print("AXIS",axisX, axisY, axisZ)
+    image_segmentated = thresholding(image)
 
-    st.write('You selected Option 1')
+    # Plot image
+    fig, ax = plt.subplots()
+    ax.imshow(image_segmentated[axisX, axisY, axisZ])
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
+    
 elif selected_segmentation_option == 'Region Growing':
     st.write('You selected Option 2')
 else:
