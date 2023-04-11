2a0e709c8415ed74145c4b07db3f6e2b0e4df8fd
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-04-10 22:52:41 -0500

    feat: improve style, add clustering, updating RegionGrowing algorithm

--- index.py
+++ index.py
@@ -2,78 +2,164 @@ import streamlit as st # To create interface
 import nibabel as nib # To read images
 import numpy as np # Operations
 import matplotlib.pyplot as plt # Plots
+import os
 
 st.title("Procesamiento Digital de Imágenes")
 
-filenames = ["T1.nii", "IR.nii", "FLAIR.nii"]
+# Create a file uploader widget
+uploaded_file = st.file_uploader("Selecciona una imagen")
 
-# Get image
-image_input = st.selectbox("Selecciona una imagen", filenames)
-image_data = nib.load(f'./images/1/{image_input}.gz')
-image = image_data.get_fdata()
+# If a file was uploaded
+if uploaded_file is not None:
+    # Get the contents of the file as a byte string
+    file_contents = uploaded_file.getvalue()
 
-# Create three columns for the inputs
-col1, col2, col3 = st.columns(3)
+    # # Set the file extension based on the uploaded file type
+    file_extension = os.path.splitext(uploaded_file.name)[1]
 
-# Add a numeric input field to each column
-with col1:
-    axisX = st.number_input("Eje X", value=-1, step=1)
-with col2:
-    axisY = st.number_input("Eje Y", value=-1, step=1)
-with col3:
-    axisZ = st.number_input("Eje Z", value=0, step=1)
+    if (file_extension==".gz"):
+        file_extension = ".nii.gz"
+    else: 
+        file_extension = ".nii"
 
-if sum([axisX <= -1, axisY <= -1, axisZ <= -1]) != 2:
-    st.warning("Dos ejes deben tener exáctamente el valor de -1")
+    # Save the byte string to a temporary file with the correct extension
+    with open("temp_file" + file_extension, "wb") as f:
+        f.write(file_contents)
 
-if axisX == -1:
+    # Get the path of the temporary file
+    path = os.path.abspath("temp_file" + file_extension)
+
+    # Load the NIfTI image using nibabel
+    image_data = nib.load(path)
+    image = image_data.get_fdata()
+
+    # Create two columns for the axis inputs
+    col1, col2 = st.columns(2)
+
+    # Add a dropdown to select an axis and a slider to select an index
+    with col1:
+        filenames = ["Eje X", "Eje Y", "Eje Z"]
+        axis_selected = st.selectbox("Selecciona un eje", filenames)
+
+        if axis_selected == "Eje X":
+            axis_shape = image.shape[0]
+
+        if axis_selected == "Eje Y":
+            axis_shape = image.shape[1]
+
+        if axis_selected == "Eje Z":
+            axis_shape = image.shape[2]
+
+
+    with col2:
+        print(axis_shape)
+        axis_value = st.slider(label="Posición", min_value=0, max_value=axis_shape, step=1, value=1)
+        # axis_value = st.slider(label="Posición", 0, axis_shape, step=1 )
+    
+    # Axis adjusment
     axisX = slice(None)
-if axisY == -1:
     axisY = slice(None)
-if axisZ == -1:
     axisZ = slice(None)
 
-segmentation_options = ['Thresholding', 'Region Growing', 'K-means']
-selected_segmentation_option = st.radio('Selecciona una técnica de segmentación', segmentation_options)
-
-# Create segmentation button
-segmentation_button_clicked = st.button("Crear segmentación")
-
-# Algorithms
-def thresholding(image, tol = 1, tau = 20):
-
-    while True:
-        # print(tau)
-
-        segmentation = image >= tau
-        mBG = image[np.multiply(image > 10, segmentation == 0)].mean()
-        mFG = image[np.multiply(image > 10, segmentation == 1)].mean()
-
-        tau_post = 0.5 * (mBG + mFG)
-
-        if np.abs(tau - tau_post) < tol:
-            break
-        else:
-            tau = tau_post
-
-    return segmentation
-
-
-if selected_segmentation_option == 'Thresholding' and segmentation_button_clicked:
-    # Create the plot using imshow
-    print("AXIS",axisX, axisY, axisZ)
-    image_segmentated = thresholding(image)
+    if axis_selected == "Eje X":
+        axisX = axis_value
+        axisY = slice(None)
+        axisZ = slice(None)
+    if axis_selected == "Eje Y":
+        axisY = axis_value
+        axisX = slice(None)
+        axisZ = slice(None)
+    if axis_selected == "Eje Z":
+        axisZ = axis_value
+        axisX = slice(None)
+        axisY = slice(None)
 
     # Plot image
     fig, ax = plt.subplots()
-    ax.imshow(image_segmentated[axisX, axisY, axisZ])
-
-    # Display the plot using Streamlit
+    ax.imshow(image[axisX, axisY, axisZ])
     st.pyplot(fig)
-    
-elif selected_segmentation_option == 'Region Growing':
-    st.write('You selected Option 2')
-else:
-    st.write('You selected Option 3')
 
-# print(image)
+    # Create a container
+    container = st.container()
+
+    segmentation_options = ['Thresholding', 'Region Growing', 'Clustering']
+    selected_segmentation_option = st.radio('Selecciona una técnica de segmentación', segmentation_options)
+
+    # Create segmentation button
+    segmentation_button_clicked = st.button("Crear segmentación")
+
+    # Algorithms
+    def thresholding(image, tol = 1, tau = 20):
+
+        while True:
+            # print(tau)
+
+            segmentation = image >= tau
+            mBG = image[np.multiply(image > 10, segmentation == 0)].mean()
+            mFG = image[np.multiply(image > 10, segmentation == 1)].mean()
+
+            tau_post = 0.5 * (mBG + mFG)
+
+            if np.abs(tau - tau_post) < tol:
+                break
+            else:
+                tau = tau_post
+
+        return segmentation
+
+    def clustering(image, tol = 1, tau = 150):
+        k1 = np.amin(image)
+        k2 = np.mean(image)
+        k3 = np.amax(image)
+        # print(k1, k2, k3)
+
+        for i in range(0,3):
+            d1 = np.abs(k1 - image)
+            d2 = np.abs(k2 - image)
+            d3 = np.abs(k3 - image)
+
+            segmentation = np.zeros_like(image)
+            segmentation[np.multiply(d1 < d2, d1 < d3)] = 0
+            segmentation[np.multiply(d2 < d1, d2 < d3)] = 1
+            segmentation[np.multiply(d3 < d1, d3 < d2)] = 2
+
+            k1 = image[segmentation == 0].mean()
+            k2 = image[segmentation == 1].mean()
+            k3 = image[segmentation == 2].mean()
+        
+        return segmentation
+
+    if selected_segmentation_option == 'Thresholding' and segmentation_button_clicked:
+        # Create the plot using imshow
+        image_segmentated = thresholding(image)
+
+        # Plot image
+        fig, ax = plt.subplots()
+        ax.imshow(image_segmentated[axisX, axisY, axisZ])
+
+        # Display the plot using Streamlit
+        st.pyplot(fig)
+        
+    elif selected_segmentation_option == 'Region Growing' and segmentation_button_clicked:
+        # Create the plot using imshow
+        image_segmentated = clustering(image)
+
+        # Plot image
+        fig, ax = plt.subplots()
+        ax.imshow(image_segmentated[axisX, axisY, axisZ])
+
+        # Display the plot using Streamlit
+        st.pyplot(fig)
+
+    elif selected_segmentation_option == 'Clustering' and segmentation_button_clicked:
+        # Create the plot using imshow
+        image_segmentated = clustering(image)
+
+        # Plot image
+        fig, ax = plt.subplots()
+        ax.imshow(image_segmentated[axisX, axisY, axisZ])
+
+        # Display the plot using Streamlit
+        st.pyplot(fig)
+
+    # print(image)
